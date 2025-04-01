c2113387098acdce0ad7c33ae7667c5987914161
Meen <64070198@IT.KMITL.AC.TH>
2025-04-01 19:08:41 +0700

    Update vite.config.ts

--- my-project/vite.config.ts
+++ my-project/vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    chunkSizeWarningLimit: 1000, // ขนาด chunk warning ที่ต้องการ (KB)
+    chunkSizeWarningLimit: 5000, // ขนาด chunk warning ที่ต้องการ (KB)
   },
 })
 // import { defineConfig } from "vite";
